9c91fe46ebd0925d103a8ced85ba71879eb2466b
Justin Siow <77658010+justinsiowqi@users.noreply.github.com>
2025-01-08 09:43:14 +0800

    Connect V1 V1 Nodes

---
+++ relation_properties/connect_v1_v1_nodes.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+def calculate_similarity(word_term1_notag, word_term2_notag):
+    
+    # Download the MiniLM Model (Highest Accuracy + Fastest)
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    
+    # Encode Each Word in the Word Term No Tag List
+    word_term1_embeddings = {}
+    for word in word_term1_notag:
+        word_term1_embeddings[word] = model.encode(word)
+
+    word_term2_embeddings = {}
+    for word in word_term2_notag:
+        word_term2_embeddings[word] = model.encode(word)
+    
+    # Compare Each Word and Get the Similarity Scores
+    sim_scores = []
+    for word1, embedding1 in word_term1_embeddings.items():
+        for word2, embedding2 in word_term2_embeddings.items():
+            score = model.similarity(embedding1, embedding2).item()
+            sim_scores.append((word1, word2, score))
+        
+    return sim_scores
+
+def extract_similar_words(sim_scores, threshold=0.65):
+
+    # Create a Dataframe
+    df = pd.DataFrame(sim_scores, columns=["Question 1", "Question 2", "Similarity_Score"])
+    
+    # Extract Word Terms Above Threshold
+    df["Above_Threshold"] = df["Similarity_Score"] > threshold
+    filtered_df = df[df["Above_Threshold"] == True].drop(columns=["Above_Threshold"])
+    
+    sim_words = filtered_df.to_dict("records")
+    
+    return sim_words
